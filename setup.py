3d8c4cc8f0a4356c16f44541741eebe5bca8911d
Dmitry Orlov <me@mosquito.su>
2020-03-23 23:02:36 +0300

    [fix] classifiers

--- setup.py
+++ setup.py
@@ -63,4 +63,21 @@ setup(
             define_macros=[("NO_PNG", "1")]
         ),
     ],
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Topic :: Internet',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
 )
