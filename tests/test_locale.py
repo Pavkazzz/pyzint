b94bd034ad79b9b0b1116347ced10800ebac591a
pavkazzz <me@pavkazzz.ru>
2021-12-11 22:25:23 +0300

    Fix test_locale line length

--- tests/test_locale.py
+++ tests/test_locale.py
@@ -5,5 +5,5 @@ from pyzint import zint
 
 def test_ru_locale_svg():
     locale.setlocale(locale.LC_NUMERIC, "ru_RU.utf8")
-    xml = zint.Zint("test", zint.BARCODE_AZTEC, height=200, border_width=4).render_svg().decode()
-    assert "," not in xml
+    barcode = zint.Zint("test", zint.BARCODE_AZTEC, height=200, border_width=4)
+    assert "," not in barcode.render_svg().decode()
